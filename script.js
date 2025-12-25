caa776983120d69e2a681dadd47f5372fbf90956
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-25 16:24:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const form = document.getElementById("contactForm");
+const status = document.getElementById("formStatus");
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  status.textContent = "Sending message...";
+  status.style.color = "#2563eb";
+
+  setTimeout(() => {
+    status.textContent = "Message sent successfully!";
+    status.style.color = "green";
+    form.reset();
+  }, 1000);
+});
